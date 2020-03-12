f232756ac0fa510771dd3d3f31f911592f0bd598
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-03-12 14:41:36 -0400

    Repeated Transactions

--- backend/database/seeds/TransactionTableSeeder.php
+++ backend/database/seeds/TransactionTableSeeder.php
@@ -4,6 +4,7 @@ use Carbon\Carbon;
 use App\Model\Category;
 use App\Model\Transaction;
 use Illuminate\Database\Seeder;
+use App\Model\Useful\DateConversion;
 
 class TransactionTableSeeder extends Seeder
 {
@@ -15,27 +16,35 @@ class TransactionTableSeeder extends Seeder
     public function run()
     {
         factory(Transaction::class, 50)->create();
+        //51
         factory(Transaction::class)->create([
+            'description' => 'Income',
             'type' => 'Income',
             'due_at' => Carbon::now()->toDateString(),
             'payed' => 0,
         ]);
         factory(Transaction::class)->create([
+            'description' => 'Income',
             'type' => 'Income',
             'due_at' => Carbon::now()->toDateString(),
             'payed' => 1,
         ]);
+        //53
         factory(Transaction::class)->create([
+            'description' => 'Expense',
             'type' => 'Expense',
             'due_at' => Carbon::now()->toDateString(),
             'payed' => 0,
         ]);
         factory(Transaction::class)->create([
+            'description' => 'Expense',
             'type' => 'Expense',
             'due_at' => Carbon::now()->toDateString(),
             'payed' => 1,
         ]);
+        //55
         factory(Transaction::class)->create([
+            'description' => 'Transfer',
             'type' => 'Expense',
             'is_transfer' => true,
             'amount' => 50,
@@ -44,6 +53,7 @@ class TransactionTableSeeder extends Seeder
             'first_transaction' => 55
         ]);
         factory(Transaction::class)->create([
+            'description' => 'Transfer',
             'type' => 'Income',
             'is_transfer' => true,
             'amount' => 50,
@@ -51,5 +61,20 @@ class TransactionTableSeeder extends Seeder
             'payed' => 1,
             'first_transaction' => 55
         ]);
+        //57
+        $repeatedDate = Carbon::now()->firstOfMonth()->toDateString();
+        for($i=0; $i<3; $i++) {
+            factory(Transaction::class)->create([
+                'description' => 'Repeat',
+                'type' => 'Income',
+                'amount' => 50,
+                'due_at' => $repeatedDate,
+                'payed' => 0,
+                'first_transaction' => 57
+            ]);
+
+            $repeatedDate = DateConversion::newDateByPeriod($repeatedDate, 'Biweekly')->toDateString();
+        }
+        //60
     }
 }
